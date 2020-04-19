3f85ab82f8e0b3c1c85f1dc5a49e3628b211b418
Jon-Morten Kristiansen <jon-morten@urbansharing.com>
2020-04-19 17:34:38 +0200

    Tailwind CSS and mocked list of stations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,82 @@
-const IndexPage = () => "Hello!";
+import React from "react";
+
+type ID = string;
+
+type Station = {
+  id: ID;
+  address: string;
+  bikesAvailable: number;
+  docksAvailable: number;
+  inService: boolean;
+  name: string;
+};
+
+const mocks: Station[] = [
+  {
+    id: "627",
+    address: "Skøyen Stasjon",
+    bikesAvailable: 7,
+    docksAvailable: 5,
+    inService: true,
+    name: "Skøyen Stasjon",
+  },
+  {
+    id: "623",
+    address: "7 Juni Plassen",
+    bikesAvailable: 4,
+    docksAvailable: 8,
+    inService: true,
+    name: "7 Juni Plassen",
+  },
+  {
+    id: "610",
+    address: "Sotahjørnet",
+    bikesAvailable: 4,
+    docksAvailable: 9,
+    inService: true,
+    name: "Sotahjørnet",
+  },
+];
+
+const Container: React.FC = ({ children }) => (
+  <div className="max-w-screen-sm mx-auto p-4">{children}</div>
+);
+
+const StationList: React.FC<{ stations: Station[] }> = ({ stations }) => {
+  if (!stations.length) {
+    return <>Ingen stativer i systemet!</>;
+  }
+
+  return (
+    <div>
+      {stations.map((station) => (
+        <div key={station.id}>
+          <strong>{station.name}</strong>
+          {station.name !== station.address && <em>station.address</em>}
+          <p>🚲{station.bikesAvailable} sykler</p>
+          <p>🔓{station.docksAvailable} ledige låser</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const IndexPage = () => {
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [stations, setStations] = React.useState<Station[]>(mocks);
+
+  return (
+    <Container>
+      <h1 className="text-3xl mb-2">Bysykkelstativer</h1>
+
+      {error && <p>Kunne ikke hente info om stativene! Prøv igjen senere.</p>}
+
+      {loading && <p>Henter info om stativene ...</p>}
+
+      {!loading && !error && <StationList stations={stations} />}
+    </Container>
+  );
+};
 
 export default IndexPage;
